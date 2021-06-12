84c596a4da85efe28eb99fd3ab0a173eea87c010
ngsanthosh <ngsanthosh657@gmail.com>
2021-06-12 12:42:31 +0530

    Add serializers and modify views

--- api/serializers.py
+++ api/serializers.py
@@ -3,4 +3,4 @@ from rest_framework import serializers
 class Studentserializer(serializers.Serializer):
     name=serializers.CharField(max_length=256)
     roll=serializers.IntegerField()
-    mark=serializers.IntegerField()
+    marks=serializers.IntegerField()
--- api/views.py
+++ api/views.py
@@ -7,17 +7,19 @@ from api import serializers
 
 import json
 # Create your views here.
+
 class Student:
-    def __init__(self, name, roll, mark):
+    def __init__(self, name, roll, marks):
         self.name=name
         self.roll=roll
-        self.mark=mark
+        self.marks=marks
 
 @api_view()
 def apiusers(request):
-
-    student1=Student("Santhosh",1,99)
-    student2=Student("Gokul",2,98)
+    student1=Student("Santhosh",1,98)
+    student2=Student("Gokul",2,99)
     student3=Student("Kumar",3,97)
-    response=serializers.Studentserializer([student1,student2,student3],many=True)
+    response=serializers.Studentserializer([student1,student2,student3], many=True)
     return Response(response.data)
+    
+
