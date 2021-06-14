547db54077d350aa90df461c8ce8fae7d80afd21
ngsanthosh <ngsanthosh657@gmail.com>
2021-06-14 19:55:37 +0530

    Saw more on REst API generic views

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('users/', views.apiusers),
-    path('articles/', views.articleApi),
-    path('createarticles/', views.createArticleApi),
-
+    # path('users/', views.apiusers),
+    path('articles/', views.ArticlesListView.as_view()),
+    path('articles/<int:pk>', views.ArticlesDetailView.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -3,46 +3,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
-from api import serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.generics import ( ListAPIView, RetrieveAPIView, UpdateAPIView, RetrieveUpdateAPIView, ListCreateAPIView)
 
-from api import models
+from api import serializers, models
 
-import json
-# Create your views here.
+class ArticlesListView(ListCreateAPIView):
+    queryset=models.Article.objects.all()
+    serializer_class= serializers.ArticleSerializer
 
-class Student:
-    def __init__(self, name, roll, marks):
-        self.name=name
-        self.roll=roll
-        self.marks=marks
+class ArticlesDetailView(RetrieveUpdateAPIView):
+    queryset=models.Article.objects.all()
+    serializer_class= serializers.ArticleSerializer
 
-@api_view()
-def apiusers(request):
-    student1=Student("Santhosh",1,98)
-    student2=Student("Gokul",2,99)
-    student3=Student("Kumar",3,97)
-    response=serializers.Studentserializer([student1,student2,student3], many=True)
-    return Response(response.data)
-
-@api_view()
-def articleApi(request):
-    articles=models.Article.objects.all()
-    response=serializers.ArticleSerializer(articles, many = True)
-    return Response(response.data)
-
-@api_view(['POST'])
-def createArticleApi(request):
-    body=json.loads(request.body)
-    response=serializers.ArticleSerializer(data =body)
-
-    if response.is_valid():
-
-        inst = response.save()
-        response=serializers.ArticleSerializer(inst)
-        return Response(response.data)
-
-    # print(json.loads(request.body))
-    return Response(response.errors)
 
 
 
